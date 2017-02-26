c5180f585331ebda5a0bd5d24767e79e3796d77a
kigihan <kigihan77@gmail.com>
2017-02-27 00:19:33 +0800

    b

--- runlazybot.py
+++ runlazybot.py
@@ -141,7 +141,7 @@ def crawPage(url, push_rate, soup):
                     print("        HighPost: " + URL)
                     res_post = requests.get(URL, verify=False)
                     soup_post = BeautifulSoup(res_post.text, "html.parser")
-                    img_links = soup_post.select("a")[0]["href"]
+                    img_links = soup_post.select("a")["href"]
                     print("    ImgLinks: " + img_links)
                     img_links_list = []
                     for link in img_links:
