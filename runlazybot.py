61ab71b4a500390562a836f9955e750c4e0e26ad
kigihan <kigihan77@gmail.com>
2017-02-27 05:12:19 +0800

    2

--- runlazybot.py
+++ runlazybot.py
@@ -224,7 +224,7 @@ def callback():
                 # )
                 line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text = carousel_template_message)
+                TextSendMessage(text = carousel_template_message[0])
                 )
 
     return 'OK'
