d335851a89a1086a98df71d950987816c1b73d0c
kigihan <kigihan77@gmail.com>
2017-02-28 01:43:19 +0800

    s

--- runlazybot_FrameTest.py
+++ runlazybot_FrameTest.py
@@ -476,6 +476,7 @@ def ptt_simple_board(simple_board_name, simple_push_rate, filter_simple):
     #print("    soup>>>" + soup.prettify())
     #class=btn wide
     #抓最新-1頁的連結
+    LatestPageURI = []
     LatestPageURI = soup.select('.btn.wide')[1]['href']
     #print("    URI>>> " + LatestPageURI)
     #從連接拆出最新-1頁數
