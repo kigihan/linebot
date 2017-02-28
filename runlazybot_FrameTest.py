60249b6ac305c7771e049e7aac4a77c859770f1e
kigihan <kigihan77@gmail.com>
2017-02-28 08:48:55 +0800

    b

--- runlazybot_FrameTest.py
+++ runlazybot_FrameTest.py
@@ -283,6 +283,7 @@ def callback():
                 #用完把list內容刪掉，達到重置的效果，不然舊的紀錄還在，結果累積推文數最高的那篇
         # article_list = []
         # article_list_sorted = []
+        del all_template_message
         del article_list[:]
         del article_list_sorted[:]
         simple_board_name = ""
