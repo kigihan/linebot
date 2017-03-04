03c9c538c231107f5a1fd3f1539034f7f4a45a6f
kigihan <kigihan77@gmail.com>
2017-03-04 18:08:51 +0800

    w

--- runlazybot_v3.0.py
+++ runlazybot_v3.0.py
@@ -444,6 +444,7 @@ def simple_craw_page(url, push_rate, soup, filter_simple, simple_filter_type):
                 post_date = r_ent.find(class_="date").text
                 post_author = r_ent.find(class_="author").text
                 print("....date: " + post_date + " | author: " + post_author + "\n")
+                print("Now: ", datetime.datetime.now())
                 date_now_utc = datetime.datetime.now()
                 print("[+] UTC Time is: " + date_now_utc + "\n")
                 date_now_taiwan = date_now_utc + 8*HOUR
