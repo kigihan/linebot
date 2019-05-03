ef1bfe8a0a5da80c6736561763d0fcccb0060474
kigihan <kigihan77@gmail.com>
2019-05-03 18:12:55 +0800

    yn

--- runlazybot_v4.1.py
+++ runlazybot_v4.1.py
@@ -765,7 +765,7 @@ def json_104_proc(json, kw):
 	for i in range(len(job_locale_count_sorted)):
 		tmp = []
 		for jl, js in job_total_matrix:
-			print(jl, js, job_total_matrix[i][0], job_total_matrix[i][1])
+			#print(jl, js, job_total_matrix[i][0], job_total_matrix[i][1])
 			if jl == job_total_matrix[i][0]:
 				tmp.append(int(js))
 		job_summ[i].append(max(tmp))
@@ -778,6 +778,7 @@ def job_locale_message(summ):
 	#print(summ)
 	for x in summ:
 		locale_msg += "\n" + x[0] + ": " + str(x[1]) + " 筆    $" + str(x[2])
+		print(x[1], x[2])
         
 def job_locale_message_ori(locale_count):
 	locale_msg = "\n"
