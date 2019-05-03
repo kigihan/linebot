24e855bc18e0491e69818a16bf416a8161350a5c
kigihan <kigihan77@gmail.com>
2019-05-03 13:52:08 +0800

    fix brackets

--- runlazybot_v4.1.py
+++ runlazybot_v4.1.py
@@ -759,8 +759,9 @@ def json_104_proc(json, kw):
         
 def job_locale_message(locale_count):
 	locale_msg = ""
+	print(locale_count)
 	for x in locale_count:
-		locale_msg += "\n" + x + ": " + str(locale_count(x)) + " 筆"
+		locale_msg += "\n" + x + ": " + str(locale_count[x]) + " 筆"
 	return(locale_msg)
 
 if __name__ == "__main__":
